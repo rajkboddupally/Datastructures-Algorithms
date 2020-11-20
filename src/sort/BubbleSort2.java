e97002a4c584ee8c83d2505bccc46e55aec9fb9f
rajkumarboddupally <rajkumarboddupally@rajkumars-mbp>
2020-11-19 20:42:22 -0500

    bubblesort

---
+++ src/sort/BubbleSort2.java
@@ -0,0 +1,25 @@
+package sort;
+
+public class BubbleSort2 {
+
+	public static void main(String[] args) {
+		int[] intArray = { 26, -6, 3, 65, 12, 0, 5 };
+
+		for (int lastUnsortedIndex = intArray.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex--) {
+			for (int i = 0; i < lastUnsortedIndex; i++) {
+				if (intArray[i] > intArray[i + 1])
+					swap(intArray, i, i + 1);
+			}
+		}
+
+		for (int j = 0; j < intArray.length; j++)
+			System.out.print(intArray[j] + " ");
+	}
+
+	private static void swap(int[] intArray, int i, int j) {
+		int temp = intArray[i];
+		intArray[i] = intArray[j];
+		intArray[j] = temp;
+	}
+
+}
