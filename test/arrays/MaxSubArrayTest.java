148eae31e0f9eaf574dee41e42d4d7c20dd674a2
Raj Kumar Boddupally <rajkboddupally@gmaill.com>
2021-02-05 21:18:08 -0500

    test

--- test/arrays/MaxSubArrayTest.java
+++ test/arrays/MaxSubArrayTest.java
@@ -1,45 +1,43 @@
 package arrays;
 
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class MaxSubArrayTest {
     MaxSubArray maxSubArray = new MaxSubArray();
+
     @Test
     void maxSubArray() {
 
-        int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
-        Assert.assertEquals(6,maxSubArray.maxSubArray(nums));
+        int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        Assert.assertEquals(6, maxSubArray.maxSubArray(nums));
     }
 
     @Test
     void maxSubArray1() {
 
         int[] nums = {1};
-        Assert.assertEquals(1,maxSubArray.maxSubArray(nums));
+        Assert.assertEquals(1, maxSubArray.maxSubArray(nums));
     }
 
     @Test
     void maxSubArray2() {
 
         int[] nums = {0};
-        Assert.assertEquals(0,maxSubArray.maxSubArray(nums));
+        Assert.assertEquals(0, maxSubArray.maxSubArray(nums));
     }
 
     @Test
     void maxSubArray3() {
 
         int[] nums = {-2147483647};
-        Assert.assertEquals(-2147483647,maxSubArray.maxSubArray(nums));
+        Assert.assertEquals(-2147483647, maxSubArray.maxSubArray(nums));
     }
 
     @Test
     void maxSubArray4() {
 
-        int[] nums = {-2,-4,-1};
-        Assert.assertEquals(-1,maxSubArray.maxSubArray(nums));
+        int[] nums = {-2, -4, -1};
+        Assert.assertEquals(-1, maxSubArray.maxSubArray(nums));
     }
 }
