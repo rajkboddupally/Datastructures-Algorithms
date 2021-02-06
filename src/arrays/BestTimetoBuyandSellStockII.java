8d5570d241df2d3b72c04e915a2cd7dab6c233c5
Raj Kumar Boddupally <rajkboddupally@gmaill.com>
2021-02-06 15:58:16 -0500

    BestTimetoBuyandSellStockII.java - updated simplest method

--- src/arrays/BestTimetoBuyandSellStockII.java
+++ src/arrays/BestTimetoBuyandSellStockII.java
@@ -61,7 +61,7 @@ public class BestTimetoBuyandSellStockII {
         return tempProfit;
     }
 
-    public int maxProfit(int[] prices) {
+    public int maxProfit_V1(int[] prices) {
         int maxProfit = 0;
         int buy = 0;
         int sell = 0;
@@ -81,5 +81,16 @@ public class BestTimetoBuyandSellStockII {
             maxProfit += sell - buy;
         }
         return maxProfit;
+    }
+
+    public int maxProfit(int[] prices) {
+        int profit = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1]) {
+                profit += prices[i] - prices[i - 1];
+            }
+        }
+        return profit;
     }
 }
