3a4e9646945f294c9afbdd0097a577f1344eccf1
Raj Kumar Boddupally <rajkboddupally@gmaill.com>
2021-02-13 22:26:02 -0500

    Merging old programs from local machines

---
+++ src/bst/TwoSum.java
@@ -0,0 +1,28 @@
+package bst;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class TwoSum {
+
+    public static void main(String[] args) {
+    }
+
+
+}
