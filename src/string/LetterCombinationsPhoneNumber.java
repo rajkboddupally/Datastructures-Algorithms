d0b09635b16273524ff53b7e7563d4d293b2d1bd
Raj Kumar Boddupally <rajkboddupally@gmaill.com>
2021-07-21 14:13:26 +0530

    Algorithm - String CountandSay

--- src/string/LetterCombinationsPhoneNumber.java
+++ src/string/LetterCombinationsPhoneNumber.java
@@ -9,26 +9,35 @@ public class LetterCombinationsPhoneNumber {
     public static void main(String[] args) {
         LetterCombinationsPhoneNumber lcp = new LetterCombinationsPhoneNumber();
 
-        List<String> list = lcp.letterCombinations("23");
+        List<String> list = lcp.letterCombinations("3485769");
+        /*
         for (String str : list) {
             System.out.println(str);
         }
+        */
+        System.out.println("Size " + list.size());
 
     }
 
     public List<String> letterCombinations(String digits) {
 
-        LinkedList<String> ans = new LinkedList<>();
-        ans.add("");
-        String[] map = {"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        LinkedList<String> ans = new LinkedList<String>();
 
-        while (ans.peek().length() != digits.length()) {
-            String remove = ans.remove();
-            System.out.println("remove len " + remove.length());
-            String myMap = map[digits.charAt(remove.length()) - '0'];
-            System.out.println("myMap " + myMap);
-            for (char c : myMap.toCharArray()) {
-                ans.addLast(remove + c);
+        if (digits.isEmpty())
+            return ans;
+
+        String[] mapping = {"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+
+        ans.add("");
+
+        for (int i = 0; i < digits.length(); i++) {
+            int digit = Character.getNumericValue(digits.charAt(i));
+            while (ans.peek().length() == i) {
+                String s = ans.remove();
+
+                for (char c : mapping[digit].toCharArray()) {
+                    ans.add(s + c);
+                }
             }
         }
         return ans;
