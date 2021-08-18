28e02d40efda691d9d06088bb813a3b1b2cac6d0
Raj Kumar Boddupally <rajkboddupally@gmaill.com>
2021-08-17 22:34:27 -0400

    Algorithm - InsertionSort optimized

--- src/sort/InsertionSort.java
+++ src/sort/InsertionSort.java
@@ -2,27 +2,37 @@ package sort;
 
 public class InsertionSort {
     public static void main(String[] args) {
-        int[] arr = {6, 3, -7, 9, 2};
+        Integer[] arr = {6, 3, -7, 9, 2};
         insertionSort(arr);
 
-        for (int k = 0; k < arr.length; k++)
-            System.out.print(arr[k] + " ");
+        print(arr);
 
     }
 
-    private static void insertionSort(int[] arr) {
-
+    private static void insertionSort(Integer[] arr) {
         for (int i = 1; i < arr.length; i++) {
-            for (int j = i; j > 0; j--) {
-                if (arr[j] < arr[j - 1])
-                    swap(arr, j, j - 1);
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0) {
+                if (arr[j] > key)
+                    swap(arr, j, j + 1);
+                else
+                    break;
+                j--;
             }
+            arr[j + 1] = key;
         }
-  }
+    }
 
-    private static void swap(int[] arr, int i, int j) {
+    private static void swap(Integer[] arr, int i, int j) {
         int temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
+    }
+
+    private static <T> void print(T[] a) {
+        for (T t : a)
+            System.out.print(t + " ");
+        System.out.println();
     }
 }
